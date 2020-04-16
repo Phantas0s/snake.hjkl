c00abbbe83f3860bcc5c8b83fed6a395d50b3048
matthieu <matthieu.cneude@gmail.com>
2020-04-16 18:46:16 +0200

    Improve amazing snake game with score, restart when dying and levels

--- src/cljs/snake/core.cljs
+++ src/cljs/snake/core.cljs
@@ -9,7 +9,10 @@
 
 (def states
   (atom {;; canvas object
-         :game/speed 450
+         :game/speed 150
+         :game/level nil ;set by level-reset
+         :game/score nil
+
          :canvas/element  (dom/getElement "canvas")
          :canvas/container-element (dom/getElement "#main")
          :canvas/ctx  (.getContext (dom/getElement "canvas") "2d")
@@ -20,14 +23,19 @@
          :canvas-unit/width  32                 ; :canvas/width / 20
          :canvas-unit/height 32 ;               ; :canvas/height / 20
          ;; snake object
-         :snake/body '([9 9] [8 9] [7 9]) ; [x y]
-         :snake/direction [0 1]          ; default direction, see `keycode->direction`
+         :snake/body nil ;set by level-reset
+         :snake/direction nil ;set by level-reset
          :snake/border 2                  ; border size
          :snake/body-color "lime"         ; snake's body color
          :snake/food nil                  ; when `nil`, regenerate it
          :snake/food-color "red"          ; the color of food
          :snake/alive true                ; when `false`, stop game loop
-         }))
+         :food/points 10
+         :wall/wall-color "brown"}))
+
+(def levels {:level-1 {:walls nil}
+             :level-2 {:walls [[12 13] [12 14] [12 15]]}
+             :level-3 {}})
 
 (defn print-states
   "Print current game states on console.(For debugging purpose)"
@@ -35,6 +43,36 @@
   (.log js/console "-------------------------------")
   (.log js/console (with-out-str (pprint @states)))
   (.log js/console "-------------------------------\n"))
+
+(defn canvas-reset
+  []
+  (let [{:keys [:canvas/ctx :canvas/width :canvas/height :canvas/background-color]} @states]
+    (aset ctx "fillStyle" background-color)
+    (.fillRect ctx
+               0
+               0
+               width
+               height)))
+
+(defn level-reset
+  []
+  (canvas-reset)
+  (swap! states assoc
+         :snake/body '([9 9] [8 9] [7 9])
+         :snake/direction [0 1]
+         :snake/food nil))
+
+(defn score-reset
+  []
+  (swap! states assoc-in [:game/score] 0))
+
+(defn game-reset
+  []
+  (swap! states assoc
+         :game/level 1
+         :snake/alive true)
+  (level-reset)
+  (score-reset))
 
 (defn axis-add
   [[x1 y1] [x2 y2]]
@@ -55,6 +93,21 @@
                width
                height)))
 
+(defn draw-text
+  "Draw some text"
+  [id text]
+  (set! (.-innerHTML (.getElementById js/document id)) text))
+
+(defn draw-wall
+  [wall-units color]
+  (when-not (nil? wall-units)
+    (doseq [w wall-units]
+      (draw-canvas-unit w color))))
+
+(defn draw-wall-level
+  [level]
+  (draw-wall (get-in levels [(keyword (str "level-" level)) :walls]) "black"))
+
 (defn resize-canvas
   "Resize the canvas according to the states"
   []
@@ -65,8 +118,8 @@
 (defn keycode->direction
   "Convert JavaScript keycode to direction array"
   [keycode]
-  (get {goog.events.KeyCodes.J [0 -1] ;up
-        goog.events.KeyCodes.K [0 1] ;down
+  (get {goog.events.KeyCodes.K [0 -1] ;up
+        goog.events.KeyCodes.J [0 1] ;down
         goog.events.KeyCodes.H [-1 0] ;left
         goog.events.KeyCodes.L [1 0]} ;right
        keycode nil))
@@ -79,8 +132,6 @@
   [event]
   (let [{:keys [:snake/direction]} @states
         new-direction (keycode->direction (.-keyCode event))]
-    (println "EVENT: " (.-keyCode event))
-    (println "DIRECTION / NEW" direction new-direction)
     (when (and new-direction (not (opposite-direction? direction new-direction)))
       (swap! states assoc-in [:snake/direction] new-direction))))
 
@@ -113,37 +164,70 @@
             (swap! states assoc-in [:snake/food] food)
             (draw-canvas-unit food food-color)
             food)
-          (recur ([(rand-int max-x) (rand-int max-y)])))))))
+          (recur (food [(rand-int max-x) (rand-int max-y)])))))))
+
+(defn snake-dead?
+  [snake-head]
+  (or
+   (snake-collision? snake-head)
+   (out-of-boundary? snake-head)))
+
+(defn add-points
+  [points]
+  (swap! states update-in [:game/score] + points))
+
+; TODO to improve that - might use a watch?
+(defn next-level
+  [score]
+  (let [{:keys [:game/level]} @states]
+    (when (> score (* level 10))
+      (swap! states update-in [:game/level] + 1)
+      (level-reset)
+      (draw-wall-level (+ level 1))
+      (js/alert (str "Level " (+ level 1) " - Are you READY?")))))
 
 (defn game-loop
   []
-  (let [{:keys [:game/speed :canvas/background-color :snake/body :snake/body-color :snake/direction]} @states
+  (let [{:keys [:game/speed
+                :game/score
+                :food/points
+                :canvas/background-color
+                :snake/body
+                :snake/body-color
+                :snake/direction]} @states
         head (axis-add (first body) direction)
         tail (last body)]
+    (draw-text "score" score)
     (generate-food)
-    (when (snake-collision? head)
-      (js/alert "You're DEAD!")
-      (swap! states assoc-in [:snake/alive] false))
-    (when (out-of-boundary? head)
-      (js/alert "Snake is out of boundery at :" head)
-      (swap! states assoc-in [:snake/alive] false))
+    (if (snake-dead? head)
+      (do
+        (js/alert "You're DEAD!!!")
+        (swap! states assoc-in [:snake/alive] false)
+        (game-reset)))
     (when (:snake/alive @states)
       (draw-canvas-unit head body-color)
       (if (eat-food? head)
-        (swap! states assoc
-               :snake/food nil
-               :snake/body (conj body head))
+        (do
+          (swap! states assoc
+                 :snake/food nil
+                 :snake/body (conj body head))
+          (add-points points))
         (do
           (draw-canvas-unit tail background-color)
           (swap! states assoc-in [:snake/body] (-> (conj body head) drop-last))))
+      (next-level score)
       (js/window.setTimeout (fn [] (game-loop)) speed))))
 
 (defn init
   []
-  (resize-canvas)
-  (events/removeAll js/document)
-  (events/listen js/document goog.events.EventType.KEYDOWN on-keydown)
-  (game-loop))
+  (game-reset)
+  (let [{:keys [:game/level]} @states]
+    (resize-canvas)
+    (events/removeAll js/document)
+    (events/listen js/document goog.events.EventType.KEYDOWN on-keydown)
+    (js/alert (str "Level " level " - Are you READY?"))
+    (draw-wall-level level)
+    (game-loop)))
 
 ; (resize-canvas)
 ; (draw-canvas-unit [0 0] "red")
